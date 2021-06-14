7be2bc79e6ba246225efd0095ac1fc6a2dadc0a5
ElderGr <gbielrs@gmail.com>
2021-06-14 20:18:02 -0300

    feat: adicionados teste E2E para a pagina de signIn

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -87,7 +87,7 @@ const SignIn: React.FC = () => {
               name="password"
               icon={FiLock}
               type="password"
-              placeholder="Password"
+              placeholder="Senha"
             />
 
             <Button type="submit">Entrar</Button>
